faf9b8994306f7683072a814a0cf4c241dcf801a
nabin <nkbhaie2017@gmail.com>
2024-03-25 11:39:49 -0600

    add function to display radio button click value

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -3,12 +3,16 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.TextView;
 import android.widget.RadioButton;
 
+import java.text.DecimalFormat;
+
 public class MainActivity extends AppCompatActivity {
 
-    // declare the variables
+    // declare variables
     private TextView displayRadioButton; // to display radio button value
 
    // radio buttons
@@ -17,11 +21,16 @@ public class MainActivity extends AppCompatActivity {
     private RadioButton junior;
     private RadioButton senior;
 
+    private TextView buttonValue;
+
     // int values
     int one = 1;
     int two = 2;
     int three = 3;
     int four = 4;
+
+    // variable to hold a selected class variable value
+    int selectedRadioButtonValue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,22 +43,58 @@ public class MainActivity extends AppCompatActivity {
         junior = findViewById(R.id.radioButtonJunior);
         senior = findViewById(R.id.radioButtonSenior);
 
-        // set the values
-        if(firstYearStd.isChecked()){
-            firstYearStd.setText(one);
-        }else if(sophomore.isChecked()){
-            sophomore.setText(two);
-        }else if(junior.isChecked()){
-            junior.setText(three);
-        } else if(senior.isChecked()){
-            senior.setText(four);
-        }
+        // Use variable binding instead
+//        firstYearStd = binding.radioButtonFirst;
+//        sophomore = binding.radioButtonSophomore;
+//        junior = binding.radioButtonJunior;
+//        senior = binding.radioButtonSenior;
 
-        // access the values -> text view
-
+        buttonValue = findViewById(R.id.buttonValue); // to check assigned values
     }
 
     public void onRadioButtonClick(View v){
+        if(firstYearStd.isChecked()){
+            selectedRadioButtonValue = one;
+            buttonValue.setText("1");
+        }else if(sophomore.isChecked()){
+            selectedRadioButtonValue = two;
+            buttonValue.setText("2");
+        }else if(junior.isChecked()){
+            selectedRadioButtonValue = three;
+            buttonValue.setText("3");
+        } else if(senior.isChecked()){
+            selectedRadioButtonValue = four;
+            DecimalFormat df = new DecimalFormat("0");
+            buttonValue.setText(df.format(selectedRadioButtonValue));
+        }
 
     }
+
+   // Log.i("info", selectedRadioButtonValue);
+
+    // Add OnCheckedChangeListener to each radio button
+//    firstYearStd.setOnCheckedChangeListener((buttonView, isChecked) -> {
+//        if (isChecked) {
+//            classValue = 1;
+//        }
+//    });
+//
+//    radioButtonSophomore.setOnCheckedChangeListener((buttonView, isChecked) -> {
+//        if (isChecked) {
+//            classValue = 2;
+//        }
+//    });
+//
+//    radioButtonJunior.setOnCheckedChangeListener((buttonView, isChecked) -> {
+//        if (isChecked) {
+//            classValue = 3;
+//        }
+//    });
+//
+//    radioButtonSenior.setOnCheckedChangeListener((buttonView, isChecked) -> {
+//        if (isChecked) {
+//            classValue = 4;
+//        }
+//    });
+
 }
